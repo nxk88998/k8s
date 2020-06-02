4be27226be147dd4bf0f583676951f899382b3cd
nxk88998 <444985876@qq.com>
2020-06-02 16:04:46 +0800

    Initial commit

--- push.sh
+++ push.sh
@@ -1,8 +1,8 @@
-git config --global user.name "陆伟"
+git config --global user.name "nxk88998"
 git config --global user.email "444985876@qq.com"
 
 git init
-git remote add origin http://192.168.1.24/lw/k8s.git
+git remote add origin https://github.com/nxk88998/k8s-.git
 git add .
 git commit -m "Initial commit"
 git push -u origin master
